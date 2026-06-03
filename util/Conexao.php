d6f9c67171be9de46b7744866a0b4bdc733ec71b
brenda14k <brendam.ifpr2024@gmail.com>
2026-06-03 14:14:52 -0300

    Add files via upload

---
+++ util/Conexao.php
@@ -0,0 +1,28 @@
+<?php
+
+class Conexao
+{
+    private static ?PDO $conexao = null;
+    public static function getConexao(): PDO
+    {
+        if (self::$conexao == null) {
+            //Criar a conexão
+            $opcoes = array(
+                //Define o charset da conexão
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+                //Define o tipo do erro como exceção
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                //Define o tipo do retorno das consultas
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            );
+
+            self::$conexao = new PDO(
+                "pgsql:host=localhost;port=5432;dbname=bebidas",
+                "postgres",
+                "123456",
+                $opcoes
+            );
+        }
+        return self::$conexao;
+    }
+}
